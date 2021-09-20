88be6589813a27c8fb9fe7fff425160e2ed1df51
James Judge <jjudge.developer83@gmail.com>
2021-09-20 00:16:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# beenaturalacres.com
---
+++ brb/index.php
@@ -0,0 +1,17 @@
+<html>
+        <head>
+                <title></title>
+                <!-- favicon -->
+        </head>
+        <body>
+                <div id="logo">
+                        <img src="./img/bee_natural_address.png"/>
+                </div>
+                <div id="message">
+                        <p>
+                                We're down for a scheduled system upgrade. If you'd like to place an order please check back on Monday as our nerds should have it worted out by then..
+                        </p>
+                </div>
+        </body>
+</html>
+
